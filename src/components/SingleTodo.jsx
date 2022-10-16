cccfe5df19ec2f5f042ad92abaaf54df770de51c
Joshowens16 <joshowens16@gmail.com>
2022-10-15 19:51:59 -0700

    done

--- src/components/SingleTodo.jsx
+++ src/components/SingleTodo.jsx
@@ -49,7 +49,7 @@ const SingleToDo = ({ todo, filter }) => {
             />
             <div style={done ? strikeStyle : normalList}>{todo}</div>
         </div>
-        ) : (' ')
+        ) : null
     }  else if (filter === 'active') {
         return !done ? (
             <div style={singleTodo}>
@@ -59,7 +59,7 @@ const SingleToDo = ({ todo, filter }) => {
             />
             <div style={done ? strikeStyle : normalList}>{todo}</div>
         </div>
-        ) : (' ') 
+        ) : null 
     } else {
         return (
             <div style={singleTodo}>
