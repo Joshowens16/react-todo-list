0c9015ed4d85c166b6470d98b8cf04cefb76bf1e
Joshowens16 <joshowens16@gmail.com>
2022-10-16 00:09:00 -0700

    broke out components

--- src/components/FilterPicker.jsx
+++ src/components/FilterPicker.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 
+const singleTodo = {
+    display: "flex",
+    flexdirection: "row",
+    borderBottom: "1px solid black",
+    margin: "10px",
+    fontFamily: "'Roboto', arial",
+}
+const strikeStyle = {
+	textDecorationLine: 'line-through',
+    fontStyle: "italic",
+    opacity: '20%',
+    fontFamily: "'Roboto', arial",
+    marginLeft: "5px",
+    letterSpacing: "1px"
+};
+const normalList = {
+    fontFamily: "'Roboto', arial",
+    marginLeft: "5px",
+    letterSpacing: "1px"
+}
 
+const notCompleted = {
+    borderRadius: "50%",
+    border:"1px solid black",
+    height: "15px"
+}
+const completed = {
+    backgroundColor:"#1E90FF",
+    color: "white",
+    borderRadius: "50%",
+    height: "15px",
+    border:"1px solid black",
+}
 
 const filterPicker = (props) => {
     return (
-        <div style={props.singleTodo}>
+        <div style={singleTodo}>
         <button
             onClick={props.clickToDo}
-            style={!props.done ? props.notCompleted : props.completed}
+            style={!props.done ? notCompleted : completed}
         />
-        <div style={props.done ? props.strikeStyle : props.normalList}>{props.todo}</div>
+        <div style={props.done ? strikeStyle : normalList}>{props.todo}</div>
     </div>
     )
 }
--- src/components/SingleTodo.jsx
+++ src/components/SingleTodo.jsx
@@ -1,42 +1,6 @@
 import React, { useState } from 'react';
 import FilterPicker from './FilterPicker';
 
-//styling components
-const singleTodo = {
-    display: "flex",
-    flexdirection: "row",
-    borderBottom: "1px solid black",
-    margin: "10px",
-    fontFamily: "'Roboto', arial",
-}
-const strikeStyle = {
-	textDecorationLine: 'line-through',
-    fontStyle: "italic",
-    opacity: '20%',
-    fontFamily: "'Roboto', arial",
-    marginLeft: "5px",
-    letterSpacing: "1px"
-};
-const normalList = {
-    fontFamily: "'Roboto', arial",
-    marginLeft: "5px",
-    letterSpacing: "1px"
-}
-
-const notCompleted = {
-    borderRadius: "50%",
-    border:"1px solid black",
-    height: "15px"
-}
-const completed = {
-    backgroundColor:"#1E90FF",
-    color: "white",
-    borderRadius: "50%",
-    height: "15px",
-    border:"1px solid black",
-}
-
-//singleTodo, clickToDo, done, notCompleted, completed, todo, strikeStyle, normalList
 const SingleToDo = (props) => {
 	const [done, setDone] = useState(false);
 	function clickToDo() {
@@ -45,38 +9,26 @@ const SingleToDo = (props) => {
 
     if (props.filter === 'complete'){
         return done ? (
-            <FilterPicker singleTodo={singleTodo} 
+            <FilterPicker 
             clickToDo={clickToDo} 
             done={done} 
-            notCompleted={notCompleted} 
-            completed={completed} 
             todo={props.todo} 
-            strikeStyle={strikeStyle} 
-            normalList={normalList} 
             />
         ) : null
     }  else if (props.filter === 'active') {
         return !done ? (
-            <FilterPicker singleTodo={singleTodo} 
+            <FilterPicker 
             clickToDo={clickToDo} 
             done={done} 
-            notCompleted={notCompleted} 
-            completed={completed} 
             todo={props.todo} 
-            strikeStyle={strikeStyle} 
-            normalList={normalList} 
             />
         ) : null 
     } else {
         return (
-            <FilterPicker singleTodo={singleTodo} 
+            <FilterPicker 
             clickToDo={clickToDo} 
             done={done} 
-            notCompleted={notCompleted} 
-            completed={completed} 
-            todo={props.todo} 
-            strikeStyle={strikeStyle} 
-            normalList={normalList} 
+            todo={props.todo}
             />
             )
     }
